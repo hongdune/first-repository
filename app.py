1e51e329c56064cac905f25af015c32f0f07db36
hongdune <68838427+hongdune@users.noreply.github.com>
2024-12-03 00:52:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CI/CD Pipeline!/nIt's test for deploy!"
+    return "Hello, CI/CD Pipeline! Update Test!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
